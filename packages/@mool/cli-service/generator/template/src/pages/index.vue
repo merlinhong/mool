b6a0e81b8a4fb32a678a16135bcee7de30ef939d
merlinhong <hj9710122021@163.com>
2025-02-26 23:22:40 +0800

    feat:优化插件

--- packages/@mool/cli-service/generator/template/src/pages/index.vue
+++ packages/@mool/cli-service/generator/template/src/pages/index.vue
@@ -1,12 +1,36 @@
 <template>
-    <HelloWorld msg="Welcome to mool.js"/>
-    <h1>Welcome to mool.js</h1>
-</template>
-<script setup lang="ts">
-</script>
-<style scoped>
-h1 {
-    font-size: 2em;
+    <div class="home">
+      <h1>Welcome to MoolJs</h1>
+      <div class="img">
+        <img src="@/assets/mooljs.png" alt="" class="mooljs" />
+        <img src="@/assets/vue.png" alt="" class="vue" />
+      </div>
+    </div>
+  </template>
+  <script setup lang="ts"></script>
+  <style scoped>
+  .home {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin: auto;
+    width: 50vw;
+    height: 50vh;
+  }
+  h1 {
+    font-size: 3em;
     color: #42b983;
-}
-</style>
+  }
+  .img {
+    display: flex;
+    justify-content: center;
+  }
+  .mooljs,
+  .vue {
+    width: 200px;
+    height: 200px;
+    margin-top: 20px;
+  }
+  </style>
+  
