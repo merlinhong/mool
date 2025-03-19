e4f057bbf876659fda1451b8390cbd4a970211c7
merlinhong <hj9710122021@163.com>
2025-03-19 23:19:34 +0800

    feat:修复bug

--- packages/@mool/cli-service/lib/preset/app/router/index.js
+++ packages/@mool/cli-service/lib/preset/app/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory,RouterView} from 'vue-router'
 import routes from '~pages';
 const router = createRouter({
   history: createWebHashHistory(),
-  routes:routes
+  routes,
 })
 export default router
 export {RouterView}
